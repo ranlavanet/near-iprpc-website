00e64a3548866f8f16e888ddd9c00bbb4587824e
Ran Mishael <ran@lavanet.xyz>
2024-02-27 16:24:56 +0100

    update website image

--- components/sendTxPageComponent.js
+++ components/sendTxPageComponent.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Container from "./container";
-import heroImg from "../public/img/test1.png";
+import heroImg from "../public/img/Default_create_a_3d_volcanic_earth_where_instead_of_the_sea_th_2_d989a65d-508f-4cf6-8cfd-7e85bd655167_0.png";
 import React, { useState } from 'react';
 import TxTabs from "./txTabs"
 
